e5b77a4678e8f168a269e701af7ce0402ab8d393
Ahmed Ibrahim <iahmediibrahim@gmail.com>
2022-04-29 23:19:10 +0200

    init routing and navbar

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import About from './AboutPage'
+import Home from './HomePage'
+import Error from './ErrorPage'
+import PrivateRoute from './PrivateRoute'
+import Products from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+import Cart from './CartPage'
+import Checkout from './CheckoutPage'
+export { About, Home, Error, PrivateRoute, Products, SingleProduct, Cart, Checkout }
