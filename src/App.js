81147574e7ce5213cf12ca04274445f965f8a0ea
Ahmed Ibrahim <iahmediibrahim@gmail.com>
2022-05-02 02:19:47 +0200

    upgrade to v18 and router v6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
 import { About, Home, Error, PrivateRoute, Products, SingleProduct, Cart, Checkout } from './pages'
 
@@ -8,27 +8,15 @@ function App() {
 		<Router>
 			<Navbar />
 			<Sidebar />
-			<Switch>
-				<Route exact path='/'>
-					<Home />
-				</Route>
-				<Route exact path='/about'>
-					<About />
-				</Route>
-				<Route exact path='/cart'>
-					<Cart />
-				</Route>
-				<Route exact path='/products'>
-					<Products />
-				</Route>
-				<Route exact path='/products/:id' children={<SingleProduct />} />
-				<Route exact path='/checkout'>
-					<Checkout />
-				</Route>
-				<Route exact path='*'>
-					<Error />
-				</Route>
-			</Switch>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/about' element={<About />} />
+				<Route path='/cart' element={<Cart />} />
+				<Route path='/products' element={<Products />} />
+				<Route path='/products/:id' element={<SingleProduct />} />
+				<Route path='/checkout' element={<Checkout />} />
+				<Route path='*' element={<Error />} />
+			</Routes>
 			<Footer />
 		</Router>
 	)
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { ProductsProvider } from './context/products_context'
@@ -7,10 +6,11 @@ import { FilterProvider } from './context/filter_context'
 import { CartProvider } from './context/cart_context'
 import { UserProvider } from './context/user_context'
 import { Auth0Provider } from '@auth0/auth0-react'
-
-ReactDOM.render(
+import { createRoot } from 'react-dom/client'
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(
 	<ProductsProvider>
 		<App />
 	</ProductsProvider>,
-	document.getElementById('root'),
 )
