3c1b8ba63a001a726ad3e5d147f49fd664d6e30e
Ahmed Ibrahim <iahmediibrahim@gmail.com>
2022-05-03 23:46:32 +0200

    sort buttons

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -26,8 +26,9 @@ export const FilterProvider = ({ children }) => {
 	useEffect(() => {
 		dispatch({ type: LOAD_PRODUCTS, products })
 	}, [products])
-
-	return <FilterContext.Provider value={{ ...state }}>{children}</FilterContext.Provider>
+	const setGridView = () => dispatch({ type: SET_GRIDVIEW })
+	const setListView = () => dispatch({ type: SET_LISTVIEW })
+	return <FilterContext.Provider value={{ ...state, setGridView, setListView }}>{children}</FilterContext.Provider>
 }
 // make sure use
 export const useFilterContext = () => {
--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -13,6 +13,16 @@ const filter_reducer = (state, action) => {
 	switch (action.type) {
 		case LOAD_PRODUCTS:
 			return { ...state, all_products: [...action.products], filtered_products: [...action.products] }
+		case SET_LISTVIEW:
+			return {
+				...state,
+				grid_view: false,
+			}
+		case SET_GRIDVIEW:
+			return {
+				...state,
+				grid_view: true,
+			}
 		default:
 			throw new Error(`No Matching "${action.type}" - action type`)
 	}
