3c0c034ee1d6256e8bd742ea08749b106b1b16d2
Ahmed Ibrahim <iahmediibrahim@gmail.com>
2022-05-06 21:24:59 +0200

    init localstorage

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -7,10 +7,17 @@ import {
   CLEAR_CART,
   COUNT_CART_TOTALS,
 } from '../actions'
-
+const getLocalStorage = () => {
+  let cart = localStorage.getItem('cart')
+  if (cart) {
+    return JSON.parse(localStorage.getItem('cart'))
+  } else {
+    return []
+  }
+}
 const initialState = {
-  cart: []
-  , total_items: 0,
+  cart: getLocalStorage(),
+  total_items: 0,
   total_amount: 0,
   shipping_fee: 534
 }
@@ -25,6 +32,10 @@ export const CartProvider = ({ children }) => {
   const toggleAmount = (id, color, amount, product) => dispatch({ type: TOGGLE_CART_ITEM_AMOUNT, payload: { id, color, amount, product } })
   const clearCart = () => dispatch({ type: CLEAR_CART })
   const countTotal = () => dispatch({ type: COUNT_CART_TOTALS })
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(state.cart))
+  }, [state.cart])
 
   return (
     <CartContext.Provider value={{ ...state, addToCart, removeItem, toggleAmount, clearCart, countTotal }}>{children}</CartContext.Provider>
